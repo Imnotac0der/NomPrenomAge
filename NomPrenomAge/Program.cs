db351ce09e644e787f0c7bfb62f37685e8fb9a42
33769 <33769@DESKTOP-ADKUBAA>
2022-10-12 18:10:15 +0200

    Ajoutez des fichiers projet.

---
+++ NomPrenomAge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace NomPrenomAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // déclarations des variables
+            string name = "silia";
+            string firstname = "zeraoui";
+            int age = 20;
+
+            // affichage personnalisé
+            Console.WriteLine("Bonjour " + name + " " + firstname + ", vous avez " + age + "ans. ");
+
+            Console.ReadLine();
+
+            // saisie du nom
+            Console.Write("Entrez votre nom");
+            name = string(Console.ReadLine());
+        }
+    }
+}
